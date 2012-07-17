e29b692b027d09eb4f00d11eaf71b27b73852d09
Ben Centra <ben@roncentra.com>
2012-07-16 22:20:24 -0400

    first commit, uploading files

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>CSH Spending - 2012</title>
+	<link rel="stylesheet" type="text/css" href="style.css"></link>
+</head>
+<body>
+	<header>
+		<h1>CSH Spending - 2012</h1>
+	</header>
+	<section> 
+		<h3>Budget Breakdown</h3>
+	</section>
+	<section>
+		<h3>Spending (by Committee)</h3>
+		<h3>OpComm</h3>
+		<h3>Evals</h3>
+		<h3>History</h3>
+		<h3>Imps</h3>
+		<h3>R&amp;D</h3>
+		<h3>Social</h3>
+		<h3>Misc</h3>
+	</section>
+	<footer>
+	
+	</footer>
+</body>
+</html>
---
+++ lib.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once("mysql_vars.php");
+
+function createTable($committee) {
+	//Connect to the MySQL database
+	$mysqli = new mysqli("db.csh.rit.edu","financial","f1nAnc1Al","financial");
+}
