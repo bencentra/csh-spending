2674c1f753ea636748bdcc71945a9a90bb1866e2
Ben Centra <ben@roncentra.com>
2012-07-18 00:13:17 -0400

    added getTotal()

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-require_once('lib.php');
+//Include constants and functions from 'lib.php'
+include('lib.php');
+
+//Define some variables for budget totals/percents
+$combinedTotal = 0;
+$combinedPercent = 0;
+$committeeTotal = 0;
+$committeePercent = 0;
+
 ?>
 
 <!DOCTYPE HTML>
@@ -13,24 +21,53 @@ require_once('lib.php');
 		<h1>CSH Spending - 2012</h1>
 	</header>
 	<section> 
-		<h3>Budget Breakdown</h3>
+		<h2>Budget Breakdown</h2>
+		<table>
+			<tr><td>Yearly On-Floor Dues: </td><td>$<?php echo YEARLY_TOTAL;?></td></tr>
+			<tr><td>OpComm: </td><td>$<?php echo round(YEARLY_TOTAL*BUDGET_OPCOMM);?></td></tr>
+			<tr><td>Evals: </td><td>$<?php echo YEARLY_TOTAL*BUDGET_EVALS;?></td></tr>
+			<tr><td>History: </td><td>$<?php echo YEARLY_TOTAL*BUDGET_HISTORY;?></td></tr>
+			<tr><td>Imps: </td><td>$<?php echo YEARLY_TOTAL*BUDGET_IMPS;?></td></tr>
+			<tr><td>R&amp;D: </td><td>$<?php echo YEARLY_TOTAL*BUDGET_RANDD;?></td></tr>
+			<tr><td>Social: </td><td>$<?php echo YEARLY_TOTAL*BUDGET_SOCIAL;?></td></tr>
+			<tr><td>Misc: </td><td>$<?php echo YEARLY_TOTAL*BUDGET_MISC;?></td></tr>
+			<tr><td>Accum (Rollover): </td><td>$<?php echo YEARLY_TOTAL*BUDGET_ACCUM;?></td></tr>
+		</table>
 	</section>
 	<section>
-		<h3>Spending (by Committee)</h3>
+		<h2>Totals</h2>
+		<b>Total Expenditure: </b>$<?php $combinedTotal = getTotal()+(YEARLY_TOTAL*BUDGET_ACCUM); echo $combinedTotal; ?>
+	</section>
+	<section>
+		<h2>Spending (by Committee)</h2>
 		<h3>OpComm</h3>
-		<?php createTable("opcomm"); ?>
+		<?php echo createTable("opcomm"); ?>
+		<b>Total Spent: </b>$<?php $committeeTotal = getTotal("opcomm"); echo $committeeTotal; ?><br/>
+		<b>Percentage Spent: </b><?php $committeePercent = round($committeeTotal/(YEARLY_TOTAL*BUDGET_OPCOMM),2); echo $committeePercent; ?>%
 		<h3>Evals</h3>
-		<?php createTable("evals"); ?>
+		<?php echo createTable("evals"); ?>
+		<b>Total Spent: </b>$<?php $committeeTotal = getTotal("evals"); echo $committeeTotal; ?><br/>
+		<b>Percentage Spent: </b><?php $committeePercent = round($committeeTotal/(YEARLY_TOTAL*BUDGET_EVALS),2); echo $committeePercent; ?>%
 		<h3>History</h3>
-		<?php createTable("history"); ?>
+		<?php echo createTable("history"); ?>
+		<b>Total Spent: </b>$<?php $committeeTotal = getTotal("history"); echo $committeeTotal; ?><br/>
+		<b>Percentage Spent: </b><?php $committeePercent = round($committeeTotal/(YEARLY_TOTAL*BUDGET_HISTORY),2); echo $committeePercent; ?>%
 		<h3>Imps</h3>
-		<?php createTable("imps"); ?>
+		<?php echo createTable("imps"); ?>
+		<b>Total Spent: </b>$<?php $committeeTotal = getTotal("imps"); echo $committeeTotal; ?><br/>
+		<b>Percentage Spent: </b><?php $committeePercent = round($committeeTotal/(YEARLY_TOTAL*BUDGET_IMPS),2); echo $committeePercent; ?>%
 		<h3>R&amp;D</h3>
-		<?php createTable("randd"); ?>
+		<?php echo createTable("randd"); ?>
+		<b>Total Spent: </b>$<?php $committeeTotal = getTotal("randd"); echo $committeeTotal; ?><br/>
+		<b>Percentage Spent: </b><?php $committeePercent = round($committeeTotal/(YEARLY_TOTAL*BUDGET_RANDD),2); echo $committeePercent; ?>%
 		<h3>Social</h3>
-		<?php createTable("social"); ?>
+		<?php echo createTable("social"); ?>
+		<b>Total Spent: </b>$<?php $committeeTotal = getTotal("social"); echo $committeeTotal; ?><br/>
+		<b>Percentage Spent: </b><?php $committeePercent = round($committeeTotal/(YEARLY_TOTAL*BUDGET_SOCIAL),2); echo $committeePercent; ?>%
 		<h3>Misc</h3>
-		<?php createTable("misc"); ?>
+		<?php echo createTable("misc"); ?>
+		<b>Total Spent: </b>$<?php $committeeTotal = getTotal("misc"); echo $committeeTotal; ?><br/>
+		<b>Percentage Spent: </b><?php $committeePercent = round($committeeTotal/(YEARLY_TOTAL*BUDGET_MISC),2); echo $committeePercent; ?>%
 	</section>
 	<footer>
 	
--- lib.php
+++ lib.php
@@ -2,7 +2,24 @@
 
 require_once("mysql_vars.php");
 
-function createTable($committee) 
+/**
+ * Budget-related constants
+ */
+define(YEARLY_TOTAL,5670);
+define(BUDGET_OPCOMM,.19);
+define(BUDGET_EVALS,.04);
+define(BUDGET_HISTORY,.09);
+define(BUDGET_IMPS,.09);
+define(BUDGET_RANDD,.19);
+define(BUDGET_SOCIAL,.25);
+define(BUDGET_MISC,.05);
+define(BUDGET_ACCUM,.10);
+
+/**
+ * getMysqliConnection() - Get a fresh mysqli connection object
+ * @return $mysqli - a mysqli connection
+ */
+function getMysqliConnection() 
 {
 	//Connect to the MySQL database using a mysqli object
 	$mysqli = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
@@ -10,10 +27,22 @@ function createTable($committee)
 	if ($mysqli->connect_error) {
 		die ("Connection Error: ".$mysqli->connect_error."<br/>");
 	}
+	//Return the $mysqli object
+	return $mysqli;
+}
+
+/**
+ * createTable() - Query the database for all purchases for a committee and create a table of the results
+ * @param string $committee - the committee to query data for
+ * @return string $html - the table of data created
+ */
+function createTable($committee) 
+{
+	//Get a mysqli connection
+	$mysqli = getMysqliConnection();
 	
 	//Form the query to select all entries for the given $committee
 	$query = "SELECT * FROM ".SPENDING_TABLE." WHERE committee='".$committee."'";
-	//echo $query."<br/>";
 	
 	//Make the query
 	$result = $mysqli->query($query);
@@ -23,27 +52,59 @@ function createTable($committee)
 	}
 	//Handle the result
 	else {
-		//echo "<pre>";
-		$html = createTableHeader();
+		//Form an html string containing the table
+		$html = "<table class='committee'>";
+		//Create the table headers
+		$html .= "<tr><th>Date</th><th>Item</th><th>Purchaser</th><th>Merchant</th><th>Amount</th></tr>";
+		//Loop through each row in the $result
 		while($row = $result->fetch_assoc()) {
-			//print_r($row);
+			//Add a row to the table
 			$html .= "<tr><td>".$row["date"]."</td>";
 			$html .= "<td>".$row["item"]."</td>";
 			$html .= "<td>".$row["purchaser"]."</td>";
 			$html .= "<td>".$row["merchant"]."</td>";
 			$html .= "<td>".$row["amount"]."</td></tr>";
 		}
+		//Close the table
 		$html .= "</table><br/>";
-		echo $html;
-		//echo "</pre>";
 	}
 	
 	//Close the mysqli connection
 	$mysqli->close();
-}
-
-function createTableHeader() 
-{
-	$html = "<table class='committee'><tr><th>Date</th><th>Item</th><th>Purchaser</th><th>Merchant</th><th>Amount</th></tr>";
+	
+	//Return the $html table string
 	return $html;
 }
+
+/**
+ * getTotal() - Use a mysql query to calculate the total budget spent, overall or by committee
+ * @param string $committee - null to query for total budget, string for committee to query total for
+ * @return double $total - the total spent overall/for the committee
+ */
+function getTotal($committee=null) 
+{
+	//Get a new mysqli connection
+	$mysqli = getMysqliConnection();
+	
+	//If $committee is null, query for the total budget spent
+	if ($committee == null) {
+		$query = "SELECT SUM(amount) as total FROM ".SPENDING_TABLE;
+	} 
+	//If $committee is set, query for the total spent by a committee
+	else {
+		$query = "SELECT SUM(amount) as total FROM ".SPENDING_TABLE." WHERE committee='".$committee."'";
+	}
+	//Make the query
+	$result = $mysqli->query($query);
+	//If there was an error, quit the script
+	if (!$result) {
+		die ("Query Error: ".$mysqli->error."<br/>");
+	}
+	//If not, return the total
+	else {
+		$total = $result->fetch_assoc();
+		return $total["total"];
+	}
+}
+
+?>
